f70e7e0486ade09ebe4e9b00ee7c7b5a3fe1aea0
EnsarHamzic(SJJ) <ensar.hamzic@bitconex.de>
2023-12-12 23:16:25 +0100

    page template

--- page.php
+++ page.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * The main template file
+ * The template for displaying all pages
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -26,7 +26,7 @@ get_header();
 								while( have_posts() ): the_post();
 									?>
 										<article>
-											<h2><?php the_title(); ?></h2>
+											<h1><?php the_title(); ?></h1>
 											<div><?php the_content(); ?></div>
 										</article>
 									<?php
